849d1e7f71a4a2543e18941e3c861ce5fa72d8d1
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-18 11:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+function letterMad(str)
+{
+	if(!str)return [];
+	
+    let ans={};
+    for(let i=0;i<str.length;i++){
+        ans[str.charAt(i)]=[];
+    }
+   
+
+    //we have created the map..
+
+    for(let i=0;i<str.length;i++)
+    {
+        ans[str.charAt(i)].push(i);
+    }
+
+    return ans;
+}
+
+
+let str=prompt("Enter Your String");
+alert(letterMad(str).toString());
